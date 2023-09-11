14deb74dd34209f57dbad92c0fa44de454c529b3
Harm Meiejr <harm.meijer@commercetools.com>
2023-09-11 22:55:09 +0200

    initial commit

--- delay_response.py
+++ delay_response.py
@@ -1,25 +1,27 @@
-from time import sleep
+# create token with local-test:token script in apis
+# use token here to set header in request (line 18)
+# set CONNECT_ENV=local-test in apis .env file
+# start this proxy with mitmproxy -s delay_response.py
+# start apis
+# start mc frontend with pnpm start
 from mitmproxy import http
-import json
-import logging
-import asyncio
 
-async def request(flow: http.HTTPFlow) -> None:
-    try:
-        # Decode the request body (assuming it's JSON) to a Python object
-        request_body = '' if flow.request.text == None else json.loads(flow.request.text)
-        if 'skirt' in json.dumps(request_body):
-            await asyncio.sleep(0.2)
-    except ValueError:
-        logging.info("Error: ValueError")
-        pass  # Handle JSON parsing errors here
-async def response(flow: http.HTTPFlow) -> None:
-    try:
-        request_body = '' if flow.request.text == None else json.loads(flow.request.text)
-        if 'skirt' in json.dumps(request_body):
-            # Delay the response by adding an artificial delay (e.g., 5 seconds)
-            # flow.response.delay = 5.0
-            await asyncio.sleep(0.2)
-    except ValueError:
-        logging.info("Error: ValueError")
-        pass  # Handle JSON parsing errors here
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "OPTIONS":
+        return
+    # if browser tries to connect to mc api, redirect to localhost
+    if flow.request.pretty_url.startswith("https://url_to_mc_api/proxy/connect/organization_id_selected/"):
+        flow.request.host = "localhost"
+        flow.request.port = 3000
+        flow.request.scheme = "http"
+        flow.request.path = flow.request.path.replace("/proxy/connect/organization_id_selected/", "/")
+        flow.request.headers["Authorization"] = "Bearer YOUR_TOKEN_CREATED_WITH_LOCAL_TEST:TOKEN_SCRIPT"
+        
+
+def response(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "OPTIONS":
+        return
+    if flow.request.pretty_url.startswith("http://localhost:3000/"):
+        flow.response.headers["access-control-allow-origin"] = flow.request.headers.get('Origin', 'http://localhost:3001')
+        flow.response.headers["access-control-allow-credentials"] = "true"
+        flow.response.headers["access-control-expose-headers"] = "x-refreshed-session-token"
--- start.sh
+++ start.sh
@@ -1,1 +1,19 @@
+# start server changed
 mitmproxy -s delay_response.py
+
+# curl 'https://mc-api.europe-west1.gcp.escemo.com/proxy/connect/32a0e125-fd74-4145-969e-b1662521a8c0/connectors-staged?integrationTypes=tax&limit=0&offset=20&sort=createdAt+desc' \
+# -x http://127.0.0.1:8080 \
+# -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0' \
+# -H 'Accept: application/json' \
+# -H 'Accept-Language: en-US,en;q=0.5' \
+# -H 'Accept-Encoding: gzip, deflate, br' \
+# -H 'x-application-id: __local:account' \
+# -H 'x-correlation-id: mc/67a57ae0-f899-42f9-899c-d28825bd983c/d4b97ab0-2c54-44a6-80e7-15f30f472c33' \
+# -H 'x-user-agent: unknown-http-client Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0 Application account' \
+# -H 'Origin: http://localhost:3001' \
+# -H 'Alt-Used: mc-api.europe-west1.gcp.escemo.com' \
+# -H 'Connection: keep-alive' \
+# -H 'Sec-Fetch-Dest: empty' \
+# -H 'Sec-Fetch-Mode: cors' \
+# -H 'Sec-Fetch-Site: cross-site' \
+# -H 'TE: trailers'
