97e4f17b8d9879e8546f83e7e5b1c58f5358a61d
Harm Meiejr <harm.meijer@commercetools.com>
2023-09-11 22:55:09 +0200

    ok

--- delay_response.py
+++ delay_response.py
@@ -1,25 +1,27 @@
-from time import sleep
 from mitmproxy import http
-import json
-import logging
-import asyncio
+from mitmproxy import ctx
+import re
+from config import token, host
 
-async def request(flow: http.HTTPFlow) -> None:
-    try:
-        # Decode the request body (assuming it's JSON) to a Python object
-        request_body = '' if flow.request.text == None else json.loads(flow.request.text)
-        if 'skirt' in json.dumps(request_body):
-            await asyncio.sleep(0.2)
-    except ValueError:
-        logging.info("Error: ValueError")
-        pass  # Handle JSON parsing errors here
-async def response(flow: http.HTTPFlow) -> None:
-    try:
-        request_body = '' if flow.request.text == None else json.loads(flow.request.text)
-        if 'skirt' in json.dumps(request_body):
-            # Delay the response by adding an artificial delay (e.g., 5 seconds)
-            # flow.response.delay = 5.0
-            await asyncio.sleep(0.2)
-    except ValueError:
-        logging.info("Error: ValueError")
-        pass  # Handle JSON parsing errors here
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "OPTIONS":
+        return
+        # normal production url forwared to dev or staged
+    if re.match(r"[ configure url ]", flow.request.pretty_url):
+        ctx.log.info("Monster xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        flow.request.host = host
+        flow.request.path = re.sub(r"/proxy/connect/", "/system/", flow.request.path)
+        flow.request.headers["authorization"] = "Bearer " + token
+    # if this is swagger
+    if flow.request.pretty_url.startswith(f"https://{host}"):
+        ctx.log.info("Swagger xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        flow.request.headers["authorization"] = "Bearer " + token # add the auth token to the request
+        
+
+def response(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "OPTIONS":
+        return
+    if flow.request.pretty_url.startswith("http://localhost:3000/"):
+        flow.response.headers["access-control-allow-origin"] = flow.request.headers.get('Origin', 'http://localhost:3001')
+        flow.response.headers["access-control-allow-credentials"] = "true"
+        flow.response.headers["access-control-expose-headers"] = "x-refreshed-session-token"
