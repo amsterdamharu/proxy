4270cedb1be80cef5e96c3f935593f99ddf27bc8
Harm Meiejr <harm.meijer@commercetools.com>
2023-09-11 22:55:09 +0200

    initial commit

--- delay_response.py
+++ delay_response.py
@@ -1,25 +1,33 @@
-from time import sleep
 from mitmproxy import http
-import json
-import logging
-import asyncio
+from mitmproxy import ctx
+import time
 
-async def request(flow: http.HTTPFlow) -> None:
-    try:
-        # Decode the request body (assuming it's JSON) to a Python object
-        request_body = '' if flow.request.text == None else json.loads(flow.request.text)
-        if 'skirt' in json.dumps(request_body):
-            await asyncio.sleep(0.2)
-    except ValueError:
-        logging.info("Error: ValueError")
-        pass  # Handle JSON parsing errors here
-async def response(flow: http.HTTPFlow) -> None:
-    try:
-        request_body = '' if flow.request.text == None else json.loads(flow.request.text)
-        if 'skirt' in json.dumps(request_body):
-            # Delay the response by adding an artificial delay (e.g., 5 seconds)
-            # flow.response.delay = 5.0
-            await asyncio.sleep(0.2)
-    except ValueError:
-        logging.info("Error: ValueError")
-        pass  # Handle JSON parsing errors here
+LOCAL_TARGET_HOST = "localhost"
+LOCAL_TARGET_PORT = 4000
+TOKEN="your valid manage token"
+
+
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "OPTIONS":
+        return
+    operation_name = flow.request.headers.get("x-graphql-operation-name", "")
+    if "Agentic" in operation_name and operation_name != "FetchStoresForAgenticChannels":
+        # if operation_name == "UpdateAgenticChannel":
+        #     time.sleep(5)
+        flow.request.host = LOCAL_TARGET_HOST
+        flow.request.port = LOCAL_TARGET_PORT
+        flow.request.scheme = "http"
+        flow.request.path = "/graphql"
+        flow.request.headers["authorization"] = f'Bearer {TOKEN}'
+    else:
+        ctx.log.info("NOT ROUTING TO LOCALHOST")
+        ctx.log.info(flow.request.pretty_url)
+
+def response(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "OPTIONS":
+        return
+    if flow.request.pretty_url.startswith(f"http://{LOCAL_TARGET_HOST}:{LOCAL_TARGET_PORT}/"):
+        flow.response.headers["access-control-allow-origin"] = flow.request.headers.get('Origin', f'http://{LOCAL_TARGET_HOST}:{LOCAL_TARGET_PORT}')
+        flow.response.headers["access-control-allow-credentials"] = "true"
+        flow.response.headers["access-control-allow-headers"] = "authorization, content-type"
+        flow.response.headers["access-control-expose-headers"] = "x-refreshed-session-token"
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+# start server changed
 mitmproxy -s delay_response.py
